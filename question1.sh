bc52de1f4ad9b39fe562f985aea9835a11c0efd6
Kothapally Gnana Praneeth <123532043+praneeth-0501@users.noreply.github.com>
2023-02-03 15:58:09 +0530

    Add files via upload

---
+++ question1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo $(date "+DATE: %D") #To print current date
+echo $(date "+Time: %T") #To print current Time
+echo "Username: $(whoami)" #to print username
+echo "Home Directory: $HOME" #to print Home Directory
+echo "Current working directory: $(pwd)" #to print current directory
+
