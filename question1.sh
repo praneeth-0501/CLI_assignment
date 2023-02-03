7253151e0c2525127731c289a28bf5f94620e6d8
Kothapally Gnana Praneeth <123532043+praneeth-0501@users.noreply.github.com>
2023-02-03 15:57:53 +0530

    Delete question1.sh

--- question1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Current date and time $(date)" #to print the current date and time
-echo "Username $(whoami)" #to print username
-echo "Home Directory $HOME" #to print Home Directory
-echo "Current working directory $(pwd)" #to print current directory
-
